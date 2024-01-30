5eb35c78ac3054ae09ec6866e35f2c704488b54e
Anna Kapitanova <totoshechka19@gmail.com>
2024-01-31 01:01:29 +0300

    Сделала слайдер

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    autoplay: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+
+
