cb72f46e9d3face0f16dd7d8b16f4cb47e7dcaa9
Anna Kapitanova <totoshechka19@gmail.com>
2024-02-01 16:36:53 +0300

    Сделала мобильное меню

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// ---------- Header Toggle Start ---------- //
+let menuBtn = document.getElementById("menuBtn");
+let menuList = document.querySelector(".menu");
+menuBtn.addEventListener("click", ()=> {
+    menuBtn.classList.toggle("fa-xmark");
+    menuList.classList.toggle("open");
+    document.body.classList.toggle('no-scroll')
+});
+
+let menuLinks = document.querySelectorAll(".menu__list-link");
+for (let menuLink of menuLinks) {
+    menuLink.addEventListener("click", ()=> {
+        menuBtn.classList.toggle("fa-xmark");
+        menuList.classList.toggle("open");
+        document.body.style.overflowY = 'visible';
+    });
+}
+// ---------- Header Toggle End ---------- //
+
+
 const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 3,
